4ff7bbf1860b5683a9e6b46f47beca5d2ef41bea
Sarabjeet Kumar <sarabjeetkumar@rocketmail.com>
2016-01-06 17:44:13 +0000

    Declaring interface

---
+++ oopAssignment/src/ie/gmit/sw/Resultable.java
@@ -0,0 +1,18 @@
+package ie.gmit.sw;
+
+//Interface Resultable 
+public interface Resultable {
+	
+	public abstract String getPlainText();
+	
+	public abstract void setPlainText(String plainText);
+	
+	public abstract int getKey();
+	
+	public abstract void setKey(int key);
+	
+	public abstract void setScore(double score);
+	
+	public abstract double getScore();
+
+}
